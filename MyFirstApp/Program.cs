35180f896c21e50833081feea873c0ccb2f85061
darmiina <daia@tktk.ee>
2023-05-06 23:52:56 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Rakendus küsib kasjutajal sisestada tema nime
+// Rakendus tervitab kasjutajat nimepidi
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:"); // Output
+//string - sõne
+string userName = Console.ReadLine(); // Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); // Output
+
+// String interpolation
+
+Console.WriteLine($"Hello, {userName} !"); // Output
+
